d73023e0cab489b1846bd77ec7c67ab3825a66ec
JD Ghuman <jdsghuman@gmail.com>
2020-04-13 15:33:07 -0500

    Updates

--- precache-manifest.c3a17c636acb4587fa24b5f40d22f378.js
+++ precache-manifest.c3a17c636acb4587fa24b5f40d22f378.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d8f91b5c89b8b9c6e62d9c1a4f2663b",
+    "revision": "143cdc23e92d01ec2d779a368aa5317a",
     "url": "/fartmasterflex/index.html"
   },
   {
-    "revision": "c42a94a49b7da9d71694",
-    "url": "/fartmasterflex/static/css/main.95eabc89.chunk.css"
+    "revision": "04c5468d39067f942f09",
+    "url": "/fartmasterflex/static/css/main.db96d1f5.chunk.css"
   },
   {
-    "revision": "8ca20cbd88924f7c31e8",
-    "url": "/fartmasterflex/static/js/2.8977270d.chunk.js"
+    "revision": "02c74311522c386c8640",
+    "url": "/fartmasterflex/static/js/2.b998d126.chunk.js"
   },
   {
-    "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/fartmasterflex/static/js/2.8977270d.chunk.js.LICENSE.txt"
+    "revision": "aca8f8fc88948883037dd6367532578e",
+    "url": "/fartmasterflex/static/js/2.b998d126.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c42a94a49b7da9d71694",
-    "url": "/fartmasterflex/static/js/main.7bb47234.chunk.js"
+    "revision": "04c5468d39067f942f09",
+    "url": "/fartmasterflex/static/js/main.2ab2d748.chunk.js"
   },
   {
     "revision": "ed59261c493ee52799d2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fartmasterflex/precache-manifest.20dd0668923f0282a0b6010399defd3a.js"
+  "/fartmasterflex/precache-manifest.c3a17c636acb4587fa24b5f40d22f378.js"
 );
 
 self.addEventListener('message', (event) => {
