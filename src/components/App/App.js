8565ec8aa6a7214d89303de741cb680f0015d86d
JD Ghuman <jdsghuman@gmail.com>
2020-04-13 10:52:48 -0500

    Update analytics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,22 @@ import './App.css';
 import Home from '../Home/Home';
 import About from '../About/About';
 import ProcessFart from '../ProcessFart/ProcessFart';
+import ReactGA from 'react-ga';
+import auth from './auth.ts';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+const trackingId = "UA-163519116-1";
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+});
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 const App = () => {
   return (
