77305088814bd83fc4e887536844e1e4cac0b119
JD Ghuman <jdsghuman@gmail.com>
2020-04-14 10:26:49 -0500

    Updates

---
+++ precache-manifest.27d59f98444c411230e29ad264d5f454.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d3a7b894fc3660be4d259fbbd49ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0593cb724243b6dfde53",
+    "url": "/static/css/main.db96d1f5.chunk.css"
+  },
+  {
+    "revision": "466bc251b610c10505bf",
+    "url": "/static/js/2.7685b200.chunk.js"
+  },
+  {
+    "revision": "aca8f8fc88948883037dd6367532578e",
+    "url": "/static/js/2.7685b200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0593cb724243b6dfde53",
+    "url": "/static/js/main.949e8c6a.chunk.js"
+  },
+  {
+    "revision": "573085308cc602124a9d",
+    "url": "/static/js/runtime-main.6c64cd4b.js"
+  },
+  {
+    "revision": "7f0f51b441e548157425dec8b57a37f8",
+    "url": "/static/media/bomb-drop.7f0f51b4.mp3"
+  },
+  {
+    "revision": "0c2eea460b8c2fe3c785f0fd96835a58",
+    "url": "/static/media/full-throttle.0c2eea46.mp3"
+  },
+  {
+    "revision": "46c918e78c6b490914d429995dfbb32c",
+    "url": "/static/media/hot-97.46c918e7.mp3"
+  },
+  {
+    "revision": "2e33d75c76b75a88201aecb23f54f8e3",
+    "url": "/static/media/loud-farts.2e33d75c.mp3"
+  },
+  {
+    "revision": "455e9118512101162abf1ec93344a08f",
+    "url": "/static/media/yo-check-pants.455e9118.mp3"
+  }
+]);
