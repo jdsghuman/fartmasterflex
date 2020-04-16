e408564485bba4df7872204dbc0cb384e6941d6e
JD Ghuman <jdsghuman@gmail.com>
2020-04-15 23:23:40 -0500

    Add sneak attack

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,20 +3,21 @@ import { connect } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 import Seo from '../Seo/Seo';
-import homeStyles from './Home.module.scss';
+import styles from './Home.module.scss';
 import Layout from '../Layout/Layout';
 import Button from '../Button/Button';
 import SliderItem from '../Slider/SliderItem';
 import Logo from '../Logo/Logo';
-import { fartSounds, fartTimer, makeFart, makeFartNow } from '../Utility/Data';
+import { fartSounds, fartTimer, makeFart, makeFartNow, makeFartNowSilent } from '../Utility/Data';
 import classNames from 'classnames/bind';
 import { RESET_FART, RESET_TIMER, SET_FART, SET_TIMER } from '../../redux/actionTypes';
 import Footer from '../Footer/Footer';
 import Share from '../SocialMedia/Share';
+import SneakMode from '../SneakMode/SneakMode';
 
-const cx = classNames.bind(homeStyles);
+const cx = classNames.bind(styles);
 
-const Home = ({ selectedFart, selectedTimer }) => {
+const Home = ({ selectedFart, selectedTimer, sneakMode }) => {
   let history = useHistory();
   const dispatch = useDispatch();
 
@@ -26,7 +27,11 @@ const Home = ({ selectedFart, selectedTimer }) => {
   const handleClickFart = (name) => {
     dispatch({ type: SET_FART, payload: name });
     setActiveFartRequired(false);
-    makeFartNow(name);
+    if (sneakMode) {
+      makeFartNowSilent(name);
+    } else {
+      makeFartNow(name);
+    }
   }
   const handleClickTimer = (name) => {
     dispatch({ type: SET_TIMER, payload: name });
@@ -72,11 +77,13 @@ const Home = ({ selectedFart, selectedTimer }) => {
       <div className={'heading-container'}>
         <Logo />
         <h1 className={'txt-heading'}>Fartmaster Flex</h1>
+        <SneakMode />
       </div>
-      <h3 className={homeStyles.steps}>Select fart: <span className={cx("required", {
+      <p className={cx('sneak-mode', { 'sneak-mode--active': sneakMode })}>Sneak attack</p>
+      <h3 className={styles.steps}>Select fart: <span className={cx("required", {
         "required-show": activeFartRequired
       })}>*Required</span></h3>
-      <div className={homeStyles['scroll-container']}>
+      <div className={styles['scroll-container']}>
         <SliderItem
           items={fartSounds}
           handleClick={handleClickFart}
@@ -84,7 +91,7 @@ const Home = ({ selectedFart, selectedTimer }) => {
           isFart
         />
       </div>
-      <h3 className={homeStyles.steps}>Select timer: <span className={cx("required", {
+      <h3 className={styles.steps}>Select timer: <span className={cx("required", {
         "required-show": activeTimerRequired
       })}>*Required</span></h3>
       <div className={cx('scroll-container', 'scroll-container-timer')}>
@@ -103,7 +110,7 @@ const Home = ({ selectedFart, selectedTimer }) => {
           selectedTimer={selectedTimer}
           primary
         >
-          Start Fart Mix
+          Set Fart Timer
             </Button>
       </div>
       <Share />
@@ -114,7 +121,8 @@ const Home = ({ selectedFart, selectedTimer }) => {
 
 const mapStateToProps = state => ({
   selectedFart: state.fartReducer,
-  selectedTimer: state.timerReducer
+  selectedTimer: state.timerReducer,
+  sneakMode: state.sneakModeReducer
 });
 
 export default connect(mapStateToProps)(Home);
--- src/components/SneakMode/SneakMode.js
+++ src/components/SneakMode/SneakMode.js
@@ -6,7 +6,7 @@ import styles from './SneakMode.module.scss';
 import { SET_SNEAK_MODE } from '../../redux/actionTypes';
 
 
-const SneakMode = ({ sneakModeReducer }) => {
+const SneakMode = ({ sneakMode }) => {
   const dispatch = useDispatch();
 
   const toggleSneakMode = () => {
@@ -18,14 +18,14 @@ const SneakMode = ({ sneakModeReducer }) => {
       <Icon identifier='volume'
         viewBox='0 0 512 512'
         dimensions={{ width: 25, height: 25 }}
-        fill={sneakModeReducer ? "#e82e37" : "#6c3737"}
+        fill={sneakMode ? "#e82e37" : "#6c3737"}
       />
     </div>
   )
 }
 
 const mapStateToProps = state => ({
-  sneakModeReducer: state.sneakModeReducer
+  sneakMode: state.sneakModeReducer
 });
 
 export default connect(mapStateToProps)(SneakMode);
